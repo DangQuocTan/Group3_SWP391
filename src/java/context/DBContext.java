34fbfa4f657070d569e93a45a690e1172a4b4295
DangQuocTan <tandqhe153259@fpt.edu.vn>
2022-09-20 21:14:42 +0700

    first commit

---
+++ src/java/context/DBContext.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package context;
+
+/**
+ *
+ * @author dangq
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DBContext {
+    
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+    /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+     public Connection getConnection()throws Exception {
+        String url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+    
+    /*Insert your other code right after this comment*/
+   
+    /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
+    private final String serverName = "DESKTOP-00K4NLC\\TANBEO";
+    private final String dbName = "SWP391_Project_Test";
+    private final String portNumber = "1433";
+    private final String userID = "sa";
+    private final String password = "123";
+}
