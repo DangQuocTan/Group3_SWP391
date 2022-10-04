c379f74c4cc838800d1ccb7be0c4c4f098acd372
DangQuocTan <tandqhe153259@fpt.edu.vn>
2022-10-04 20:57:01 +0700

    g

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -22,7 +22,7 @@ public class DBContext {
         String instanceName = "DESKTOP-00K4NLC\\TANBEO";
         String port = "1433";
         String uid = "sa";
-        String pwd = "admin";
+        String pwd = "123";
         String db = "SWP391_Project_Test";
         String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
         String url = "jdbc:sqlserver://" + IP + "\\" + instanceName + ":" + port
